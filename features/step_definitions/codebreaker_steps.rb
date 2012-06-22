2291b76090379cc8bd3835eba7f874060f4b2a13
liuyanzhi08 <702368372@qq.com>
2012-06-23 02:02:57 +0800

    first commit

---
+++ features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,29 @@
+class Output
+		def initialize
+				@messages ||= []
+		end
+
+		def messages
+				@messages
+		end
+
+		def puts(message)
+				@messages << message
+		end
+end
+
+def output
+		@output ||= Output.new
+end
+
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+		game = Codebreaker::Game.new(output)
+		game.start
+end
+
+Then /^I should see "(.*?)"$/ do |message|
+		output.messages.should  include(message)
+end
